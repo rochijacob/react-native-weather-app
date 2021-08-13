703eff9f88744fcde692ca149969d1870616b7f6
Rocio Jacob <rochijacob@gmail.com>
2021-08-13 13:58:58 -0300

    update state & constructors

--- App.js
+++ App.js
@@ -1,50 +1,125 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, 
-  Text, 
+import React from "react";
+import { render } from "react-dom";
+import {
+  StyleSheet,
+  Text,
   View,
   ImageBackground,
   KeyboardAvoidingView,
+  ActivityIndicator, //circular loading spinner
   Platform, // aplica estilo basado en el OS
-  TextInput 
-} from 'react-native'; //La primera declaracion trae el stylesheet api
+  StatusBar,
+} from "react-native"; //La primera declaracion trae el stylesheet api
 
-import SearchInput from './components/SearchInput';
+import SearchInput from "./components/SearchInput";
 
-import getImageForWeather from './utils/getImageForWeather';
+import { fetchLocationId, fetchWeather } from "./utils/api";
+import getImageForWeather from "./utils/getImageForWeather";
 
-export default function App() {
-  return (
-    <KeyboardAvoidingView style={styles.container} behavior="padding">
-      <ImageBackground
-source={getImageForWeather('Clear')}
-style={styles.imageContainer}
-imageStyle={styles.image}
->
-<View style={styles.detailsContainer}>
-<Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
-      <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
-      <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
-    
-      <SearchInput placeholder="Search any city" />
-</View>
-</ImageBackground>
-    </KeyboardAvoidingView>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      error: false,
+      location: " ",
+      temperature: 0,
+      weather: "",
+    };
+  }
+
+  componentDidMount() {
+    //me permite acceder a lo que se llaman React "lifecycle methods" que me muestran puntos especificos en la vida de un componente
+    this.handleUpdateLocation("San Francisco");
+  }
+
+  handleUpdateLocation = async (city) => {
+    if (!city) return;
+
+    this.setState({ loading: true }, async () => {
+      //cuando empieza a correr la funcion carga
+      try {
+        const locationId = await fetchLocationId(city);
+        const { location, weather, temperature } = await fetchWeather(
+          locationId
+        );
+
+        this.setState({
+          loading: false, //termina de cargar
+          error: false,
+          location,
+          weather,
+          temperature,
+        });
+      } catch (e) {
+        this.setState({
+          loading: false,
+          error: true,
+        });
+      }
+    });
+  };
+  render() {
+    const { loading, error, location, weather, temperature } = this.state;
+
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <StatusBar barStyle="light-content" />
+        <ImageBackground
+          source={getImageForWeather(weather)}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+          <View style={styles.detailsContainer}>
+            <ActivityIndicator animating={loading} color="white" size="large" />
+            {!loading && (
+              <View>
+                {error && (
+                  <Text style={[styles.smallText, styles.textStyle]}>
+                    Could Not load weather, please try a different city
+                  </Text>
+                )}
+
+                {!error && (
+                  <View>
+                    <Text style={[styles.largeText, styles.textStyle]}>
+                      {location}
+                    </Text>
+                    <Text style={[styles.smallText, styles.textStyle]}>
+                      {weather}
+                    </Text>
+                    <Text
+                      style={[styles.largeText, styles.textStyle]}
+                    >{`${Math.round(temperature)}°`}</Text>
+                  </View>
+                )}
+
+                <SearchInput
+                  placeholder="Search any ciy"
+                  onSubmit={this.handleUpdateLocation}
+                />
+              </View>
+            )}
+          </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
+    );
+  }
 }
 
-//Puedo usar reactnative stylesheet api 
+//Puedo usar reactnative stylesheet api
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, //el elemento va a ocupar todo el espacio de su componente "padre"
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   detailsContainer: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,0.2)',
+    justifyContent: "center",
+    backgroundColor: "rgba(0,0,0,0.2)",
     paddingHorizontal: 20,
   },
   imageContainer: {
@@ -54,34 +129,35 @@ const styles = StyleSheet.create({
     flex: 1,
     width: null,
     height: null,
-    resizeMode: 'cover',
+    resizeMode: "cover",
   },
   textStyle: {
-    textAlign:'center',
+    textAlign: "center",
     ...Platform.select({
       ios: {
-      fontFamily: 'AvenirNext-Regular',
+        fontFamily: "AvenirNext-Regular",
       },
       android: {
-      fontFamily: 'Roboto',
+        fontFamily: "Roboto",
       },
     }),
     //fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: "white",
   },
   largeText: {
-    fontSize: 44
+    fontSize: 44,
   },
   smallText: {
     fontSize: 18,
   },
   textInput: {
-    backgroundColor: '#666',
-    color: 'white',
+    backgroundColor: "#666",
+    color: "white",
     height: 40,
     width: 300,
     marginTop: 20,
     marginHorizontal: 20,
     paddingHorizontal: 10,
-    alignSelf: 'center',
-    },
+    alignSelf: "center",
+  },
 });
--- components/SearchInput.js
+++ components/SearchInput.js
@@ -3,16 +3,46 @@ import { StyleSheet, TextInput, View } from 'react-native';
 
 //Exporto un componente con un placeholder
 export default class SearchInput extends React.Component {
+    
+    constructor(props){ //inicializamos el estado del componente y sus datos
+        super(props);
+        this.state = { //accedemos al estado del componente
+            text:'',
+        }
+    }
+
+    //Update location constant cuando el usuario cambia el campo 
+    handleChageText = text => {
+        this.setState({ text });
+    }
+
+    handleSubmitEditing = () => {
+        const {onSubmit} = this.props;
+        const {text} = this.state;
+
+        if (!text) return; //nos fijamos que this.state.text no este vacio
+
+        onSubmit(text); //esta funcion corre con lo que obtiiene de props
+        this.setState({text: ''}); //borra la propiedad de texto
+    }
+
     render(){
+        const {placeholder} = this.props;
+        const {text} = this.state;
+
+
         return(
             <View style={styles.container}>
                 <TextInput 
                     autoCorrect={false}
-                    placeholder={this.props.placeholder} //me permite poner cualquier placeholder haciendo el componente reutilizable
+                    value={text}
+                    placeholder={placeholder} //me permite poner cualquier placeholder haciendo el componente reutilizable
                     placeholderTextColor='white'
                     underlineColorAndroid='transparent' //saca un subrayado automatico de android
                     style={styles.textInput}
                     clearButtonMode='always'
+                    onChangeText={this.handleChageText} //cada vez que el texto cambia se llama, desde el parent lo tengo que llamar ==> callback
+                    onSubmitEditing={this.handleSubmitEditing} 
                 />
             </View>
         )
